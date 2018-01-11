685456b3ca6e20b8197deb7f457e324f1b88826b
Cat Janowitz and Kevin Boyle <kevin@>
2018-01-11 10:50:11 -0800

    refactor constructor to account for async properties

--- js/giphy-interface.js
+++ js/giphy-interface.js
@@ -8,33 +8,14 @@ $(document).ready(function() {
     const gifs = $('#gif-search-input').val();
     $('#gif-search-input').val("");
     const newRequest = new GiphyLand(gifs);
+
+    newRequest.getGiphy(function(cheese){
+      $('.result').append(`<img src="${newArray}">`);
+    }, function(bacon){
+      $('#errors').text("There was an error processing your request. Please try again.");
+
+    }
   });
 });
 
-// COPY OF CODE:
-// $(document).ready(function() {
-//   $('form#search').submit(function(event) {
-//     event.preventDefault();
-//     $('.result').empty();
-//     const gifs = $('#gif-search-input').val();
-//     $('#gif-search-input').val("");
-//     $.ajax({
-//       url: `http://api.giphy.com/v1/gifs/search?q=${gifs}&api_key=${apiKey}&limit=50`,
-//       type: 'GET',
-//       data: {
-//         format: 'json'
-//       },
-//       success: function(thing) {
-//         let a = Math.floor(Math.random() * 45) + 1;
-//         let b = a + 5;
-//           for (var i = a; i < b; i++){
-//             console.log(i + "i");
-//             $('.result').append(`<img src="${thing.data[i].images.fixed_height.url}">`);
-//           }
-//     },
-//       error: function() {
-//         $('#errors').text("There was an error processing your request. Please try again.")
-//       }
-//     }); //ajax callback
-//   }); //form submit
-// }); //doc ready
+// response.data[i].images.fixed_height.url
--- js/giphy.js
+++ js/giphy.js
@@ -3,32 +3,32 @@ const apiKey = require('./../.env').apiKey;
 export class GiphyLand {
   constructor(inputTopic, outputGifs) {
     this.inputTopic = inputTopic;
-    this.outputGifs = this.getGiphs();
   }
 
-  getGiphs(){
+  getGiphs(success, error){//success and error are callback function names
     $.ajax({
       url: `http://api.giphy.com/v1/gifs/search?q=${this.inputTopic}&api_key=${apiKey}&limit=50`,
       type: 'GET',
       data: {
         format: 'json'
       },
+
       success: function(response) {
-        let a = Math.floor(Math.random() * 45) + 1;
-        let b = a + 5;
+
+        const a = Math.floor(Math.random() * 45) + 1;
+        const b = a + 5;
+        let newArray = [];
         for (var i = a; i < b; i++){
           console.log(i + "i");
-          $('.result').append(`<img src="${response.data[i].images.fixed_height.url}">`);
+        newArray.push(response.data[i].images.fixed_height.url);
         }
+        success(response);//callback function named success with ARGUMENT response that hasn't necessarily been grabbed yet from server
+      },
 
-        // complete: function(){
-        //
-        // }
-
-      },//success
       error: function() {
-        $('#errors').text("There was an error processing your request. Please try again.");
+        error(response);
       }
+
     });//ajax
   } //getGiphs()
-}//GetGiphy
+}//GetGiph
